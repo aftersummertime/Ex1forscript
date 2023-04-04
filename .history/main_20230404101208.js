65f68cbae5faf4abec53d3e757a33c68530f538c
aftersummertime <74532441+toilathanh28@users.noreply.github.com>
2023-04-04 10:16:59 +0700

    commit main

---
+++ .history/main_20230404101208.js
@@ -0,0 +1,21 @@
+// let arr = prompt("Nhập các mảng số nguyên, cách nhau bằng dấu cách: ").split(" ").map(Number);
+
+// let sum = 0;
+// for(i = 0; i<arr.length; i++) {
+//     sum += arr[i];
+// }
+
+// console.log(sum);
+
+// // Nhập mảng từ người dùng
+// let arr = ("Nhập mảng các số nguyên, cách nhau bằng dấu cách: ").split(" ").map(Number);
+
+// // Tính tổng các phần tử trong mảng
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// // Hiển thị kết quả tổng ra màn hình
+// console.log("Tổng của các phần tử trong mảng là:", sum);
+
